7f98509c194fcc5d88f43d6e2eb68531f509e6de
anushkachouhan029 <anushkachouhan029>
2022-12-01 22:46:06 +0530

    added ensure the player gets to exactly twinning postion

--- Program.cs
+++ Program.cs
@@ -17,29 +17,40 @@
             Console.WriteLine($"Player is at position {PlayerPosition}");
 
             Random random = new Random();
-            diceRoll = random.Next(1, 7);
-            Console.WriteLine($"\nPlayer rolled number {diceRoll} on position {PlayerPosition}");
-
-            int option = random.Next(1, 3);
-            switch (option)
+            while (PlayerPosition < WinningPosition)
             {
-                case 1:
-                    Console.WriteLine("Player landed on a Ladder");
-                    PlayerPosition += diceRoll;
-                    Console.WriteLine($"Player moves to position {PlayerPosition}");
-                    break;
-                case 2:
-                    Console.WriteLine("Player landed on a Snake");
-                    PlayerPosition -= diceRoll;
-                    Console.WriteLine($"Player moves to position {PlayerPosition}");
-                    if (PlayerPosition < 0)
-                    {
-                        Console.WriteLine("No Play, Try Again");
-                        PlayerPosition = 0;
-                    }
-                    break;
-            }
+                diceRoll = random.Next(1, 7);
+                Console.WriteLine($"\nPlayer rolled number {diceRoll} on position {PlayerPosition}");
 
+                int option = random.Next(1, 3);
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("Player landed on a Ladder");
+                        PlayerPosition += diceRoll;
+                        Console.WriteLine($"Player moves to position {PlayerPosition}");
+                        if (PlayerPosition > WinningPosition)
+                        {
+                            PlayerPosition = PlayerPosition - diceRoll;
+                            Console.WriteLine($"Player remains in position {PlayerPosition}");
+                        }
+                        break;
+                    case 2:
+                        Console.WriteLine("Player landed on a Snake");
+                        PlayerPosition -= diceRoll;
+                        Console.WriteLine($"Player moves to position {PlayerPosition}");
+                        if (PlayerPosition < 0)
+                        {
+                            Console.WriteLine("No Play, Try Again");
+                            PlayerPosition = 0;
+                        }
+                        break;
+                }
+            }
+            if (PlayerPosition == 100)
+            {
+                Console.WriteLine("==========Player Wins==========");
+            }
         }
     }
 }
